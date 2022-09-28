4bcbb0a3e5eaa46f083b9c44b63de181e66ca7d7
“Edu58” <edumuriithi58@gmail.com>
2022-09-28 15:26:17 +0300

    change phone number field

--- EmployeeManager/settings.py
+++ EmployeeManager/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'phone_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,7 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'EmployeeManager.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'employees.CustomUser'
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = PhoneField(null=True, blank=True, help_text='Your Phone Number', max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return self.email
