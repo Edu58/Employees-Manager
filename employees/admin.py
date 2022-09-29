6dd8cbb1549535d3490a64460df5be3fc0ee857d
“Edu58” <edumuriithi58@gmail.com>
2022-09-29 09:35:52 +0300

    add Department & Employee models

--- employees/admin.py
+++ employees/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Department, Employee
 
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register([
+    CustomUser,
+    Department,
+    Employee
+])
--- employees/models.py
+++ employees/models.py
@@ -1,6 +1,9 @@
+from email.policy import default
+from random import choices
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 # Create your models here.
@@ -14,3 +17,26 @@ class CustomUser(AbstractUser):
 
     def __str__(self) -> str:
         return self.email
+
+class Department(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+STATUS_CHOICES = (
+    ("Active", "Active"),
+    ("Inactive", "Inactive")
+)
+
+class Employee(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=30)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    salary = models.PositiveIntegerField(null=False, blank=False)
+    doj = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(null=False, blank=False, max_length=80)
+    status = models.CharField(choices=STATUS_CHOICES, default="Inactive", max_length=8)
+
+    def __str__(self) -> str:
+        return self.name
